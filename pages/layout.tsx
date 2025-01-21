17c61b52fcd5a81edada34ea7f190e35575721e8
Ernst Naezer <info@ernstnaezer.nl>
2025-01-21 22:31:46 +0100

    favicon

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -16,6 +16,24 @@ export default function RootLayout({
       <Head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
       <body className={`antialiased`}>{children}</body>
     </html>
