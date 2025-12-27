457728c9b4af1a5375aaf70863526053450f6c75
Ernst Naezer <info@ernstnaezer.nl>
2025-12-27 17:51:25 +0100

    Prijzen

--- sections/overview.tsx
+++ sections/overview.tsx
@@ -10,7 +10,7 @@ const projects: ArtWork[] = [
     year: 2024,
     image: "/assets/IMG_1249.png",
     copy: "Een Engelse theeroos in zacht lichtroze straalt stille elegantie uit. Met verfijnde penseelstreken en subtiel licht wordt haar delicate schoonheid vastgelegd, een tijdloos symbool van sereniteit en zachtheid.",
-    price: "€ 250,-",
+    price: "€ 275,-",
   },
   {
     title: "Rozen en hortentias",
@@ -27,7 +27,7 @@ const projects: ArtWork[] = [
     size: "50 x 70 cm",
     year: 2024,
     image: "/assets/IMG_1707.png",
-    price: "€ 350,-",
+    price: "€ 300,-",
     copy: "Een zachtroze klimroos ontvouwt zich sierlijk langs haar takken. De verfijnde tinten en vloeiende vormen vangen een gevoel van groei en romantiek, waarin natuurlijke elegantie en tedere kracht samenkomen."
   },
   {
@@ -36,7 +36,7 @@ const projects: ArtWork[] = [
     size: "40 x 50 cm",
     year: 2025,
     image: "/assets/IMG_3200.png",
-    price: "€ 225,-",
+    price: "€ 150,-",
     copy: "Een klassiek stilleven met stralende zonnebloemen in een vaas, gecombineerd met een schaaltje perziken en abrikozen. Tegen de donkerbruine achtergrond komen warmte, overvloed en een tijdloze, schilderachtige rust samen."
   },
   {
@@ -54,7 +54,7 @@ const projects: ArtWork[] = [
     size: "20 x 30 cm",
     year: 2025,
     image: "/assets/IMG_3349.png",
-    price: "€ 150,-",
+    price: "€ 50,-",
     copy: "Een pastelkrijttekening van een vaas met zonnebloemen en delphiniums. Zachte kleurovergangen en losse lijnen geven het werk een lichte, levendige uitstraling, waarin kracht en verfijning elkaar op harmonieuze wijze ontmoeten."
   },
   {
@@ -72,7 +72,7 @@ const projects: ArtWork[] = [
     size: "20 x 30 cm",
     year: 2025,
     image: "/assets/IMG_2684.png",
-    price: "€ 175,-",
+    price: "€ 50,-",
     copy: "Een verstilde tuin met witte irissen, omlijst door strak geknipte groene hagen. In de achtergrond zorgen lavendeltonen voor diepte en zachtheid, terwijl het geheel een sfeer van rust, orde en natuurlijke elegantie ademt."
   },
   {
@@ -99,7 +99,7 @@ const projects: ArtWork[] = [
     size: "20 x 30 cm",
     year: 2025,
     image: "/assets/IMG_3758.png",
-    price: "€ 150,-",
+    price: "€ 50,-",
     copy: "Deze droomachtige pasteltekeningen vangen de stralende energie van reusachtige zonnebloemen uit de zomertuin. Met zachte kleurvlakken en subtiele texturen brengt dit kunstwerk de warme gloed en de pure schoonheid van de natuur bij u binnen."
   },
   {
@@ -108,7 +108,7 @@ const projects: ArtWork[] = [
     size: "20 x 30 cm",
     year: 2025,
     image: "/assets/IMG_3890.png",
-    price: "€ 150,-",
+    price: "€ 50,-",
     copy: "Deze zachte pasteltekening viert de romantische Dahlia ‘Pink Petticoat’ uit eigen tuin. De verfijnde, zachtroze tinten en fluweelzachte details brengen een serene, zomerse sfeer en een vleugje natuurlijke elegantie direct in uw interieur."
   },
   {
@@ -117,7 +117,7 @@ const projects: ArtWork[] = [
     size: "20 x 30 cm",
     year: 2024,
     image: "/assets/IMG_1681.png",
-    price: "€ 120,-",
+    price: "€ 50,-",
     copy: "Dit sfeervolle pastelstilleven met gedroogde bieslook en alliums in drie vazen vormt een verstilde herinnering aan een prachtig voorjaar. De zachte texturen vangen de tijdloze schoonheid van de natuur en brengen blijvende warmte."
   },
 ];
